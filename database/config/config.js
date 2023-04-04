ba6f46f69a506dcb118d4d02b9d86500637523b0
Menta <gabrielmaxss@outlook.com>
2023-04-03 21:18:47 -0300

    upgrade: minor settings improvements + Postman.json of endpoints

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT } = process.env;
-const sequelize = new Sequelize(`postgres://${DB_USERNAME}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+const database = new Sequelize(`postgres://${DB_USERNAME}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   dialect: 'postgres',
   password: DB_PASSWORD,
   logging: false,
@@ -10,11 +10,11 @@ const sequelize = new Sequelize(`postgres://${DB_USERNAME}@${DB_HOST}:${DB_PORT}
 
 (async () => {
   try {
-    await sequelize.authenticate();
-    console.log(`Connected to ${sequelize.options.dialect} database.`);
+    await database.authenticate();
+    console.log(`Connected to ${database.options.dialect} database.`);
   } catch (error) {
     console.log(error);
   }
 })();
 
-module.exports = sequelize;
+module.exports = database;
